71d4584cf0b0a0760fb9a1809a2ce54b1e67a782
sadegh <clbam8@gmail.com>
2016-06-03 14:04:28 -0700

    first commit

---
+++ model.py
@@ -0,0 +1,143 @@
+# based on ideas from https://github.com/dennybritz/cnn-text-classification-tf
+
+import tensorflow as tf
+
+
+class CharCNN(object):
+    """
+    A CNN for text classification.
+    based on the Character-level Convolutional Networks for Text Classification paper.
+    """
+    def __init__(self, num_classes=2, filter_sizes=(7, 7, 3, 3, 3, 3), num_filters_per_size=256,
+                 l2_reg_lambda=0.0, sequence_max_length=1014, num_quantized_chars=69):
+
+        # Placeholders for input, output and dropout
+        self.input_x = tf.placeholder(tf.float32, [None, num_quantized_chars, sequence_max_length, 1], name="input_x")
+        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+
+        # Keeping track of l2 regularization loss (optional)
+        l2_loss = tf.constant(0.0)
+
+        # ================ Layer 1 ================
+        with tf.name_scope("conv-maxpool-1"):
+            filter_shape = [num_quantized_chars, filter_sizes[0], 1, num_filters_per_size]
+            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
+            b = tf.Variable(tf.constant(0.1, shape=[num_filters_per_size]), name="b")
+            conv = tf.nn.conv2d(self.input_x, W, strides=[1, 1, 1, 1], padding="VALID", name="conv1")
+            h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+            pooled = tf.nn.max_pool(
+                h,
+                ksize=[1, 1, 3, 1],
+                strides=[1, 1, 3, 1],
+                padding='VALID',
+                name="pool1")
+
+        # ================ Layer 2 ================
+        with tf.name_scope("conv-maxpool-2"):
+            filter_shape = [1, filter_sizes[1], num_filters_per_size, num_filters_per_size]
+            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
+            b = tf.Variable(tf.constant(0.1, shape=[num_filters_per_size]), name="b")
+            conv = tf.nn.conv2d(pooled, W, strides=[1, 1, 1, 1], padding="VALID", name="conv2")
+            h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+            pooled = tf.nn.max_pool(
+                h,
+                ksize=[1, 1, 3, 1],
+                strides=[1, 1, 3, 1],
+                padding='VALID',
+                name="pool2")
+
+        # ================ Layer 3 ================
+        with tf.name_scope("conv-3"):
+            filter_shape = [1, filter_sizes[2], num_filters_per_size, num_filters_per_size]
+            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
+            b = tf.Variable(tf.constant(0.1, shape=[num_filters_per_size]), name="b")
+            conv = tf.nn.conv2d(pooled, W, strides=[1, 1, 1, 1], padding="VALID", name="conv3")
+            h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+
+        # ================ Layer 4 ================
+        with tf.name_scope("conv-4"):
+            filter_shape = [1, filter_sizes[3], num_filters_per_size, num_filters_per_size]
+            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
+            b = tf.Variable(tf.constant(0.1, shape=[num_filters_per_size]), name="b")
+            conv = tf.nn.conv2d(h, W, strides=[1, 1, 1, 1], padding="VALID", name="conv4")
+            h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+
+        # ================ Layer 5 ================
+        with tf.name_scope("conv-5"):
+            filter_shape = [1, filter_sizes[4], num_filters_per_size, num_filters_per_size]
+            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
+            b = tf.Variable(tf.constant(0.1, shape=[num_filters_per_size]), name="b")
+            conv = tf.nn.conv2d(h, W, strides=[1, 1, 1, 1], padding="VALID", name="conv5")
+            h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+
+        # ================ Layer 6 ================
+        with tf.name_scope("conv-maxpool-6"):
+            filter_shape = [1, filter_sizes[5], num_filters_per_size, num_filters_per_size]
+            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
+            b = tf.Variable(tf.constant(0.1, shape=[num_filters_per_size]), name="b")
+            conv = tf.nn.conv2d(h, W, strides=[1, 1, 1, 1], padding="VALID", name="conv6")
+            h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+            pooled = tf.nn.max_pool(
+                h,
+                ksize=[1, 1, 3, 1],
+                strides=[1, 1, 3, 1],
+                padding='VALID',
+                name="pool6")
+
+        # ================ Layer 7 ================
+        num_features_total = 34 * num_filters_per_size
+        h_pool_flat = tf.reshape(pooled, [-1, num_features_total])
+
+        # Add dropout
+        with tf.name_scope("dropout-1"):
+            drop1 = tf.nn.dropout(h_pool_flat, self.dropout_keep_prob)
+
+        # Fully connected layer 1
+        with tf.name_scope("fc-1"):
+            W = tf.Variable(tf.truncated_normal([num_features_total, 1024], stddev=0.05), name="W")
+            # W = tf.get_variable("W", shape=[num_features_total, 1024],
+            #                     initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[1024]), name="b")
+            # l2_loss += tf.nn.l2_loss(W)
+            # l2_loss += tf.nn.l2_loss(b)
+
+            fc_1_output = tf.nn.relu(tf.nn.xw_plus_b(drop1, W, b), name="fc-1-out")
+
+        # ================ Layer 8 ================
+        # Add dropout
+        with tf.name_scope("dropout-2"):
+            drop2 = tf.nn.dropout(fc_1_output, self.dropout_keep_prob)
+
+        # Fully connected layer 2
+        with tf.name_scope("fc-2"):
+            W = tf.Variable(tf.truncated_normal([1024, 1024], stddev=0.05), name="W")
+            # W = tf.get_variable("W", shape=[1024, 1024],
+            #                     initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[1024]), name="b")
+            # l2_loss += tf.nn.l2_loss(W)
+            # l2_loss += tf.nn.l2_loss(b)
+
+            fc_2_output = tf.nn.relu(tf.nn.xw_plus_b(drop2, W, b), name="fc-2-out")
+
+        # ================ Layer 9 ================
+        # Fully connected layer 3
+        with tf.name_scope("fc-3"):
+            W = tf.get_variable("W", shape=[1024, num_classes],
+                                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+            # l2_loss += tf.nn.l2_loss(W)
+            # l2_loss += tf.nn.l2_loss(b)
+
+            scores = tf.nn.xw_plus_b(fc_2_output, W, b, name="output")
+            predictions = tf.argmax(scores, 1, name="predictions")
+        # ================ Loss and Accuracy ================
+        # CalculateMean cross-entropy loss
+        with tf.name_scope("loss"):
+            losses = tf.nn.softmax_cross_entropy_with_logits(scores, self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+        # Accuracy
+        with tf.name_scope("accuracy"):
+            correct_predictions = tf.equal(predictions, tf.argmax(self.input_y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
---
+++ preprocessing.py
@@ -0,0 +1,94 @@
+# based on ideas from https://github.com/dennybritz/cnn-text-classification-tf
+
+import numpy as np
+import json
+
+
+def load_yelp(alphabet):
+    examples = []
+    labels = []
+    with open('./yelp-review-dataset/yelp_academic_dataset_review.json') as f:
+        i = 0
+        for line in f:
+            review = json.loads(line)
+            stars = review["stars"]
+            text = review["text"]
+            text_end_extracted = extract_end(list(text.lower()))
+            padded = pad_sentence(text_end_extracted)
+            text_int8_repr = string_to_int8_conversion(padded, alphabet)
+            if stars == 1 or stars == 2:
+                labels.append([1, 0])
+                examples.append(text_int8_repr)
+            elif stars == 4 or stars == 5:
+                labels.append([0, 1])
+                examples.append(text_int8_repr)
+            i += 1
+            if i % 10000 == 0:
+                print("Instances processed: " + str(i))
+    return examples, labels
+
+
+def extract_end(char_seq):
+    if len(char_seq) > 1014:
+        char_seq = char_seq[-1014:]
+    return char_seq
+
+
+def pad_sentence(char_seq, padding_char=" "):
+    char_seq_length = 1014
+    num_padding = char_seq_length - len(char_seq)
+    new_char_seq = char_seq + [padding_char] * num_padding
+    return new_char_seq
+
+
+def string_to_int8_conversion(char_seq, alphabet):
+    x = np.array([alphabet.find(char) for char in char_seq], dtype=np.int8)
+    return x
+
+
+def get_batched_one_hot(char_seqs_indices, labels, start_index, end_index):
+    alphabet = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
+    x_batch = char_seqs_indices[start_index:end_index]
+    y_batch = labels[start_index:end_index]
+    x_batch_one_hot = np.zeros(shape=[len(x_batch), len(alphabet), len(x_batch[0]), 1])
+    for example_i, char_seq_indices in enumerate(x_batch):
+        for char_pos_in_seq, char_seq_char_ind in enumerate(char_seq_indices):
+            if char_seq_char_ind != -1:
+                x_batch_one_hot[example_i][char_seq_char_ind][char_pos_in_seq][0] = 1
+    return [x_batch_one_hot, y_batch]
+
+
+def load_data():
+    # TODO Add the new line character later for the yelp'cause it's a multi-line review
+    alphabet = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
+    examples, labels = load_yelp(alphabet)
+    x = np.array(examples, dtype=np.int8)
+    y = np.array(labels, dtype=np.int8)
+    print("x_char_seq_ind=" + str(x.shape))
+    print("y shape=" + str(y.shape))
+    return [x, y]
+
+
+def batch_iter(x, y, batch_size, num_epochs, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    # data = np.array(data)
+    data_size = len(x)
+    print("In batch iter >> data_size is: " + str(data_size))
+    num_batches_per_epoch = int(data_size/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            x_shuffled = x[shuffle_indices]
+            y_shuffled = y[shuffle_indices]
+        else:
+            x_shuffled = x
+            y_shuffled = y
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            x_batch, y_batch = get_batched_one_hot(x_shuffled, y_shuffled, start_index, end_index)
+            batch = list(zip(x_batch, y_batch))
+            yield batch
